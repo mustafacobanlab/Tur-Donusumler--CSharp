a2bdb00bdb6fa84e7374dd9b2fae90f1e8e359b2
mustafacobanlab <mustafacobantr06@gmail.com>
2025-05-28 14:17:49 +0300

    diziler 2 guncel

---
+++ diziler2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diziler2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string[] isimler = { "Ahmet", "Mehmet", "Ayşe", "Fatma" };
+
+            //foreach (string i in isimler)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            int[] sayilar = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 90 };
+            int adet = 0;
+            Console.Write("Lütfen Dizide aramak istediğiniz sayıyı giriniz: ");
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                if (input == sayilar[i])
+                {
+                    adet++;
+
+                }
+
+
+
+            }
+            if (adet > 0)
+                Console.WriteLine("Girdiğiniz Sayı Dizide " + adet + " adet vardır.");
+            else
+                Console.WriteLine("Girdiğiniz Sayı dizide mevcut değildir.");
+
+            Console.Read();
+        }
+    }
+}
